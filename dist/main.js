9253fde88b6cd452624b42d2438816c667192414
douglashellowell <doog99@gmail.com>
2019-10-18 16:25:58 +0100

    Section layout and placeholder text/img

---
+++ dist/main.js
@@ -0,0 +1,8 @@
+function getEvenNumbers(nums) {
+    /*
+      This function takes an array of integers and returns an array containing only even integers
+      */
+     return nums.filter(num => num % 2 == 0)
+  }
+
+  console.log('hi')
